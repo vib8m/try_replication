0727b9a7c01a5aad57eeef94065fd76efe70496c
Vibhor Agarwal <vibhor.agarwal@healthplix.com>
2023-04-21 15:02:15 +0530

    Second

---
+++ first.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>This is the application First Child Page</h1>
+
+
+<?php
+echo "Hello from First Page!";
+?>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>This is the application Home Page</h1>
+</br>
+<h2>Here are some sub pages</h2>
+<a href="first.php">First Page</a>
+<a href="second.php">Second Page</a>
+
+<?php
+echo "Hello from App Home Page!";
+?>
+
+</body>
+</html>
